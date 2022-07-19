89d8b87c8f73ad49ae6208763c242fb80140e439
yamamotorina <b11u0155@gmail.com>
2022-07-19 01:35:04 +0000

    stopwatch

---
+++ main.js
@@ -0,0 +1,65 @@
+  $(function(){
+    var sec = 0;
+    var min = 0;
+    var hour = 0;
+    
+    var timer;
+  
+  //start
+  $('#start').click(function(){
+    sec = 0;
+    min = 0;
+    hour = 0;
+   
+    $('#clock').html('00:00:00:00');
+    timer = setInterval(countup, 1000);
+    
+    $(this).prop('disabled',true);
+     $('#stop,#reset').prop('disabled',false);
+  });
+  
+  //stop
+  $('#stop').click(function(){
+    clearInterval(timer);
+    $(this).prop('disabled', true);
+     $('#start,#reset').prop('disabled', false);
+  });
+  
+  //reset
+  $('#reset').click(function(){
+  sec = 0;
+  min = 0;
+  hour =0 ;
+ 
+  $('#clock').html('00:00:00:00');
+    clearInterval(timer);
+    
+    $('#stop,#reset').prop('disabled', true);
+     $('#start').prop('disabled', false);
+  });
+
+//countup
+function countup()
+{
+  sec +=1;
+  
+  if (sec >10){
+    sec = 0;
+    min += 1;
+  }
+  
+  if(min >10){
+    min = 0;
+    hour +=1;
+  }
+  
+  //ume
+  sec_number = ('0' + sec).slice(-2);
+    min_number = ('0' + min).slice(-2);
+    hour_number = ('0' + hour).slice(-2);
+
+    $('#clock').html(hour_number + ':' +  min_number + ':' + sec_number);
+  }
+  
+});
+
