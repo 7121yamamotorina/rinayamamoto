e0a47b81198e4c005838bc422677a60876403309
7121yamamotorina <108596332+7121yamamotorina@users.noreply.github.com>
2022-08-02 21:59:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,40 +7,13 @@ $(".dan").on("click", function() {
   }
   else if (target_value == "=") {
     result.value = eval(result.value);
-  }   
-    if (result.value == "0") {
+  }
+  else if (result.value == "0") {
       result.value = target_value;
-    } 
-    else {
-      if (result.value.charAt(0) == "0") {
+    }
+    else if(result.value.charAt(0) == "0") {
         result.value = result.value.slice(0, 0)
       }
-      result.value += target_value;
+      else{result.value += target_value;
     }
-  }
-)
-
-function countup()
-{
-  sec +=1;
-  
-  if (sec >10){
-    sec = 0;
-    min += 1;
-  }
-  
-  if(min >59){
-    min = 0;
-    hour +=1;
-  }
-  
-  //ume
-  sec_number = ('0' + sec).slice(-2);
-    min_number = ('0' + min).slice(-2);
-    hour_number = ('0' + hour).slice(-2);
-
-    $('#clock').html(hour_number + ':' +  min_number + ':' + sec_number);
-  }
-  
-});
-
+})
