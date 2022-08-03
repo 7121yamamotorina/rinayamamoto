a0cb227b11ada3e3939a1425c05df24ce89d0816
7121yamamotorina <108596332+7121yamamotorina@users.noreply.github.com>
2022-08-03 12:48:11 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -2,21 +2,19 @@ $(".dan").on("click", function() {
   var target_value = $(event.target).text()
   let result = document.getElementById("result")
   if (target_value == "AC") {
-    result.value = "0";
+    result.value = "0"; 
     console.log("0");
   }
   else if (target_value == "=") {
     result.value = eval(result.value);
   }
-  else {
-    if (result.value == "0") {
+  else if (result.value == "0") {
       result.value = target_value;
     }
-    else {
-      if (result.value.charAt(0) == "0") {
+    else if(result.value.charAt(0) == "0") {
         result.value = result.value.slice(0, 0)
       }
-      result.value += target_value;
+    
+      else{result.value += target_value;
     }
-  }
 })
