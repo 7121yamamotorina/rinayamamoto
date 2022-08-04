7cc9cc8c45d2598e587e08a600a1d7ecd3f2079c
7121yamamotorina <108596332+7121yamamotorina@users.noreply.github.com>
2022-08-04 11:21:40 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,5 +1,6 @@
 $(".dan").on("click", function() {
   var target_value = $(event.target).text()
+  let oper = ["+","-","*","/"]
   let result = document.getElementById("result")
   if (target_value == "AC") {
     result.value = "0"; 
@@ -14,7 +15,10 @@ $(".dan").on("click", function() {
     else if(result.value.charAt(0) == "0") {
         result.value = result.value.slice(0, 0)
       }
-    
+    else if(oper.includes(result.value.slice(-1)) && oper.includes(target_value)) {
+       console.log(result.value.slice(0,-1))
+     result.value = result.value.slice(0,-1) + target_value
+   }
       else{result.value += target_value;
     }
 })
